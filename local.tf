0569365874c0621b48ffa746caf7d64fe92bdc98
Pedro Schneider <correaschneider@gmail.com>
2024-02-28 23:28:39 -0300

    Working with outputs

--- local.tf
+++ local.tf
@@ -1,8 +1,24 @@
-resource "local_file" "exemplo" {
-  filename = "exemplo.txt"
+resource "local_file" "example" {
+  filename = "example.txt"
   content  = var.my_content
+}
+
+data "local_file" "example-content" {
+  filename = "example.txt"
+}
+
+output "data-source-result" {
+  value = data.local_file.example-content.content
 }
 
 variable "my_content" {
   type = string
+}
+
+output "id-file" {
+  value = resource.local_file.example.id
+}
+
+output "my_content" {
+  value = resource.local_file.example.content
 }
