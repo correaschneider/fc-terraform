83be4fc98220df571e8ad2bf97dc034e7cd77087
Pedro Schneider <correaschneider@gmail.com>
2024-03-10 20:13:58 -0300

    Adding subnet

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -4,3 +4,29 @@ resource "aws_vpc" "new-aws_vpc" {
     "Name" = "fullcycle-vpc"
   }
 }
+
+data "aws_availability_zones" "available" {}
+output "az" {
+  value = data.aws_availability_zones.available.names
+}
+
+
+resource "aws_subnet" "new-aws_subnet-1" {
+  availability_zone = "us-east-1a"
+  vpc_id = aws_vpc.new-aws_vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    "Name" = "fullcycle-subnet-1"
+  }
+}
+
+resource "aws_subnet" "new-aws_subnet-2" {
+  availability_zone = "us-east-1b"
+  vpc_id = aws_vpc.new-aws_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    "Name" = "fullcycle-subnet-2"
+  }
+}
