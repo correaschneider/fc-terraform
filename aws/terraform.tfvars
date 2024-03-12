248bdc56036c98d4d6ca92c2cb932a7613daf02b
Pedro Schneider <correaschneider@gmail.com>
2024-03-12 17:10:53 -0300

    Adding nodes into cluster

--- aws/terraform.tfvars
+++ aws/terraform.tfvars
@@ -1,3 +1,6 @@
 prefix = "fullcycle"
 cluster_name = "curso-fc"
 cloudwatch_retention_days_log = 30
+node_desired_size = 2
+node_max_size = 4
+node_min_size = 2
--- aws/variables.tf
+++ aws/variables.tf
@@ -1,3 +1,6 @@
 variable "prefix" {}
 variable "cluster_name" {}
 variable "cloudwatch_retention_days_log" {}
+variable "node_desired_size" {}
+variable "node_max_size" {}
+variable "node_min_size" {}
