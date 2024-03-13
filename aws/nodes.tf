e1eedadab155b3acaf32cc8429595cefdf74e6e8
Pedro Schneider <correaschneider@gmail.com>
2024-03-13 18:29:05 -0300

    Adding new nodes

--- aws/nodes.tf
+++ aws/nodes.tf
@@ -43,6 +43,29 @@ resource "aws_eks_node_group" "new-node-1" {
     min_size     = var.node_min_size
   }
 
+  instance_types = ["t3.micro"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.new-node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.new-node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.new-node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_eks_node_group" "new-node-2" {
+  node_role_arn = aws_iam_role.new-node-iam.arn
+  cluster_name = aws_eks_cluster.new-cluster.name
+  node_group_name = "${var.prefix}-node-2"
+  subnet_ids = aws_subnet.subnets[*].id
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+
+  instance_types = ["t2.micro"]
+
   depends_on = [
     aws_iam_role_policy_attachment.new-node-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.new-node-AmazonEKS_CNI_Policy,
