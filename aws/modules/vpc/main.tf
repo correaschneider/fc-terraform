edbd0e5d7096f1bbee6d4d5aae2acd24e4ab9ebe
Pedro Schneider <correaschneider@gmail.com>
2024-03-13 18:36:37 -0300

    Creating vpc module

--- aws/modules/vpc/main.tf
+++ aws/modules/vpc/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "new-aws_vpc" {
+resource "aws_vpc" "aws-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     "Name" = "${var.prefix}-vpc"
@@ -6,38 +6,35 @@ resource "aws_vpc" "new-aws_vpc" {
 }
 
 data "aws_availability_zones" "available" {}
-output "az" {
-  value = data.aws_availability_zones.available.names
-}
 
-resource "aws_internet_gateway" "new-igw" {
-  vpc_id = aws_vpc.new-aws_vpc.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.aws-vpc.id
   tags = {
     "Name" = "${var.prefix}-igw"
   }
 }
 
-resource "aws_route_table" "new-tb" {
-  vpc_id = aws_vpc.new-aws_vpc.id
+resource "aws_route_table" "tb" {
+  vpc_id = aws_vpc.aws-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.new-igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
     "Name" = "${var.prefix}-rtb"
   }
 }
 
-resource "aws_route_table_association" "new-rtb-assoc" {
+resource "aws_route_table_association" "rtb-assoc" {
   count = 2
-  route_table_id = aws_route_table.new-tb.id
+  route_table_id = aws_route_table.tb.id
   subnet_id = aws_subnet.subnets.*.id[count.index]
 }
 
 resource "aws_subnet" "subnets" {
   count = 2
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id = aws_vpc.new-aws_vpc.id
+  vpc_id = aws_vpc.aws-vpc.id
   cidr_block = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
   tags = {
